2c21e8d8ea3d96d00ae376bf99143525a350ae7e
Keith Ang <keithang@Keiths-MacBook-Pro.local>
2024-08-28 21:05:30 +0800

    implement expression eval for full compiler pipeline (parse >> compile >> interpret) for numbers; setup lldb

--- Makefile
+++ Makefile
@@ -1,45 +1,48 @@
 # Compiler
 CC = gcc
-
 # Compiler flags
-CFLAGS = -Wall -Wextra -std=c17
-
+CFLAGS = -Wall -Wextra -std=c17 -I$(INC_DIR)
+# Debug flags
+DEBUG_FLAGS = -g
 # Source directory
 SRC_DIR = ./src
-
 # Include directory
 INC_DIR = ./include
-
 # Output directory
 OUT_DIR = ./build
-
 # Source files
 SOURCES = $(wildcard $(SRC_DIR)/*.c)
-
 # Object files
 OBJECTS = $(SOURCES:$(SRC_DIR)/%.c=$(OUT_DIR)/%.o)
-
 # Executable name
 EXECUTABLE = clox
 
 # Default target
-all: $(OUT_DIR) $(EXECUTABLE)
+all: $(EXECUTABLE)
 
-# Create build directory
-$(OUT_DIR):
-	mkdir -p $(OUT_DIR)
+# Debug target
+debug: CFLAGS += $(DEBUG_FLAGS)
+debug: $(EXECUTABLE)_debug
 
 # Compile
-$(OUT_DIR)/%.o: $(SRC_DIR)/%.c
-	$(CC) $(CFLAGS) -I$(INC_DIR) -c $< -o $@
+$(OUT_DIR)/%.o: $(SRC_DIR)/%.c | $(OUT_DIR)
+	$(CC) $(CFLAGS) -c $< -o $@
 
 # Link
 $(EXECUTABLE): $(OBJECTS)
-	$(CC) $(OBJECTS) -o $@
+	$(CC) $^ -o $@
+
+# Link debug version
+$(EXECUTABLE)_debug: $(OBJECTS)
+	$(CC) $(DEBUG_FLAGS) $^ -o $@
+
+# Create build directory
+$(OUT_DIR):
+	mkdir -p $@
 
 # Clean
 clean:
-	rm -rf $(OUT_DIR) $(EXECUTABLE)
+	rm -rf $(OUT_DIR) $(EXECUTABLE) $(EXECUTABLE)_debug
 
 # Phony targets
-.PHONY: all clean
+.PHONY: all debug clean
--- src/compiler.c
+++ src/compiler.c
@@ -2,10 +2,12 @@
 #include <stdlib.h>
 
 #include "chunk.h"
-#include "common.h"
 #include "compiler.h"
 #include "scanner.h"
 
+#ifdef DEBUG_PRINT_CODE
+#include "debug.h"
+#endif
 typedef struct {
   Token current;
   Token previous;
@@ -26,6 +28,14 @@ typedef enum {
   PREC_CALL,       // . ()
   PREC_PRIMARY
 } Precedence;
+
+typedef void (*ParseFn)();
+
+typedef struct {
+  ParseFn prefix;
+  ParseFn infix;
+  Precedence precedence;
+} ParseRule;
 
 Parser parser;
 Chunk *compilingChunk;
@@ -85,7 +95,52 @@ static void emitBytes(uint8_t byte1, uint8_t byte2) {
 }
 
 static void emitReturn() { emitByte(OP_RETURN); }
-static void endCompiler() { emitReturn(); }
+static void endCompiler() {
+  emitReturn();
+
+#ifdef DEBUG_PRINT_CODE
+  if (!parser.hadError) {
+    disassembleChunk(currentChunk(), "code");
+  }
+#endif
+}
+
+static void expression();
+static ParseRule *getRule(TokenType type);
+
+static void parsePrecedence(Precedence precedence) {
+  advance();
+  ParseFn prefixRule = getRule(parser.previous.type)->prefix;
+  if (prefixRule == NULL) {
+    error("Expect expression.");
+    return;
+  }
+  prefixRule();
+
+  while (precedence <= getRule(parser.current.type)->precedence) {
+    advance();
+    ParseFn infixRule = getRule(parser.previous.type)->infix;
+    infixRule();
+  }
+}
+static void expression() { parsePrecedence(PREC_ASSIGNMENT); }
+
+static void unary() {
+  TokenType operatorType = parser.previous.type;
+
+  // compile the operand.
+  parsePrecedence(PREC_UNARY);
+
+  // Emit the operator instruction.
+  switch (operatorType) {
+  case TOKEN_MINUS:
+    emitByte(OP_NEGATE); // remember stack semantics; negate instruction should
+                         // be emitted last
+    break;
+  default:
+    return;
+  }
+}
 
 static void binary() {
   TokenType operatorType = parser.previous.type;
@@ -126,19 +181,12 @@ static void binary() {
   }
 }
 
-static void parsePrecedence(Precedence precedence) {}
-static void expression() { parsePrecedence(PREC_ASSIGNMENT); }
-
 /// As far as the back end is concerned, there's literally nothing to a
 /// grouping expression. It's sole function is syntactic -- it lets you insert
 /// a lower-precedence expression where a higher precedence is expected.
 /// Thus, it has no runtime semantics on its own and therefore doesn't emit
 /// any bytecode. jthe inner call to expression() takes care of generating
 /// byte code for the expression inside the parentheses.
-static void grouping() {
-  expression();
-  consume(TOKEN_RIGHT_PAREN, "Expect ')' after expression.");
-}
 static uint8_t makeConstant(Value value) {
   int constant = addConstant(currentChunk(), value);
   if (constant > UINT8_MAX) {
@@ -150,28 +198,58 @@ static uint8_t makeConstant(Value value) {
 static void emitConstant(Value value) {
   emitBytes(OP_CONSTANT, makeConstant(value));
 }
+
 static void number() {
   double value = strtod(parser.previous.start, NULL);
   emitConstant(value);
 }
-
-static void unary() {
-  TokenType operatorType = parser.previous.type;
-
-  // compile the operand.
-  parsePrecedence(PREC_UNARY);
+static void grouping() {
   expression();
-
-  // Emit the operator instruction.
-  switch (operatorType) {
-  case TOKEN_MINUS:
-    emitByte(OP_NEGATE); // remember stack semantics; negate instruction should
-                         // be emitted last
-    break;
-  default:
-    return;
-  }
+  consume(TOKEN_RIGHT_PAREN, "Expect ')' after expression.");
 }
+ParseRule rules[] = {
+    [TOKEN_LEFT_PAREN] = {grouping, NULL, PREC_NONE},
+    [TOKEN_RIGHT_PAREN] = {NULL, NULL, PREC_NONE},
+    [TOKEN_LEFT_BRACE] = {NULL, NULL, PREC_NONE},
+    [TOKEN_RIGHT_BRACE] = {NULL, NULL, PREC_NONE},
+    [TOKEN_COMMA] = {NULL, NULL, PREC_NONE},
+    [TOKEN_DOT] = {NULL, NULL, PREC_NONE},
+    [TOKEN_MINUS] = {unary, binary, PREC_TERM},
+    [TOKEN_PLUS] = {NULL, binary, PREC_TERM},
+    [TOKEN_SEMICOLON] = {NULL, NULL, PREC_NONE},
+    [TOKEN_SLASH] = {NULL, binary, PREC_FACTOR},
+    [TOKEN_STAR] = {NULL, binary, PREC_FACTOR},
+    [TOKEN_BANG] = {NULL, NULL, PREC_NONE},
+    [TOKEN_BANG_EQUAL] = {NULL, NULL, PREC_NONE},
+    [TOKEN_EQUAL] = {NULL, NULL, PREC_NONE},
+    [TOKEN_EQUAL_EQUAL] = {NULL, NULL, PREC_NONE},
+    [TOKEN_GREATER] = {NULL, NULL, PREC_NONE},
+    [TOKEN_GREATER_EQUAL] = {NULL, NULL, PREC_NONE},
+    [TOKEN_LESS] = {NULL, NULL, PREC_NONE},
+    [TOKEN_LESS_EQUAL] = {NULL, NULL, PREC_NONE},
+    [TOKEN_IDENTIFIER] = {NULL, NULL, PREC_NONE},
+    [TOKEN_STRING] = {NULL, NULL, PREC_NONE},
+    [TOKEN_NUMBER] = {number, NULL, PREC_NONE},
+    [TOKEN_AND] = {NULL, NULL, PREC_NONE},
+    [TOKEN_CLASS] = {NULL, NULL, PREC_NONE},
+    [TOKEN_ELSE] = {NULL, NULL, PREC_NONE},
+    [TOKEN_FALSE] = {NULL, NULL, PREC_NONE},
+    [TOKEN_FOR] = {NULL, NULL, PREC_NONE},
+    [TOKEN_FUN] = {NULL, NULL, PREC_NONE},
+    [TOKEN_IF] = {NULL, NULL, PREC_NONE},
+    [TOKEN_NIL] = {NULL, NULL, PREC_NONE},
+    [TOKEN_OR] = {NULL, NULL, PREC_NONE},
+    [TOKEN_PRINT] = {NULL, NULL, PREC_NONE},
+    [TOKEN_RETURN] = {NULL, NULL, PREC_NONE},
+    [TOKEN_SUPER] = {NULL, NULL, PREC_NONE},
+    [TOKEN_THIS] = {NULL, NULL, PREC_NONE},
+    [TOKEN_TRUE] = {NULL, NULL, PREC_NONE},
+    [TOKEN_VAR] = {NULL, NULL, PREC_NONE},
+    [TOKEN_WHILE] = {NULL, NULL, PREC_NONE},
+    [TOKEN_ERROR] = {NULL, NULL, PREC_NONE},
+    [TOKEN_EOF] = {NULL, NULL, PREC_NONE},
+};
+static ParseRule *getRule(TokenType type) { return &rules[type]; }
 
 bool compile(const char *source, Chunk *chunk) {
   initScanner(source);
