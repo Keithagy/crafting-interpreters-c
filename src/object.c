0bf417d018d8ad61a54ba03d02298b3bc92f51e7
Keith Ang <keithang@Keiths-MacBook-Pro.local>
2024-09-10 15:33:51 +0800

    implement native function calling pipeline + clock() native function

--- src/object.c
+++ src/object.c
@@ -43,6 +43,12 @@ ObjFunction *newFunction() {
   return function;
 }
 
+ObjNative *newNative(NativeFn function) {
+  ObjNative *native = ALLOCATE_OBJ(ObjNative, OBJ_NATIVE);
+  native->function = function;
+  return native;
+}
+
 ObjString *takeString(char *chars, int length) {
   uint32_t hash = hashString(chars, length);
   ObjString *interned = tableFindString(&vm.strings, chars, length, hash);
@@ -81,5 +87,9 @@ void printObject(Value value) {
   case OBJ_FUNCTION:
     printFunction(AS_FUNCTION(value));
     break;
+  case OBJ_NATIVE: {
+    printf("<native fn>");
+    break;
+  }
   }
 }
--- src/vm.c
+++ src/vm.c
@@ -12,6 +12,7 @@
 #include <stdbool.h>
 #include <stdio.h>
 #include <string.h>
+#include <time.h>
 
 // The choice to have a static VM instance is a concession for the book,
 // but not necessarily a sound engineering choice for a real language
@@ -22,6 +23,10 @@
 // That way, the host app can control when and where memory for the VM is
 // allocated, run multiple VMs in parallel, etc.
 VM vm;
+
+static Value clockNative(int argCount, Value *args) {
+  return NUMBER_VAL((double)clock() / CLOCKS_PER_SEC);
+}
 
 static void resetStack() {
   vm.stackTop = vm.stack;
@@ -49,6 +54,21 @@ static void runtimeError(const char *format, ...) {
   }
   resetStack();
 }
+static void defineNative(const char *name, NativeFn function) {
+  // You're probably wondering why we push and pop the name and function on the
+  // stack. that looks weird, right? This is the kind of stuff you have to worry
+  // about when garbage collection gets involved. Both `copyString()` and
+  // `newNative()` dynamically allocate memory. That means once we have a GC,
+  // they can potentially trigger a collection. If that happens, we need to
+  // ensure the collector knows we're not done with the name and ObjFunction so
+  // that it doesn't free them out from under us. Storing them on the value
+  // stack accomplishes that.
+  push(OBJ_VAL(copyString(name, (int)strlen(name))));
+  push(OBJ_VAL(newNative(function)));
+  tableSet(&vm.globals, AS_STRING(vm.stack[0]), vm.stack[1]);
+  pop();
+  pop();
+}
 void push(Value value) {
   *vm.stackTop = value;
   vm.stackTop++;
@@ -62,6 +82,7 @@ void initVM() {
   vm.objects = NULL;
   initTable(&vm.globals);
   initTable(&vm.strings);
+  defineNative("clock", clockNative);
 }
 
 void freeVM() {
@@ -92,6 +113,13 @@ static bool callValue(Value callee, int argCount) {
     switch (OBJ_TYPE(callee)) {
     case OBJ_FUNCTION:
       return call(AS_FUNCTION(callee), argCount);
+    case OBJ_NATIVE: {
+      NativeFn native = AS_NATIVE(callee);
+      Value result = native(argCount, vm.stackTop - argCount);
+      vm.stackTop -= argCount + 1;
+      push(result);
+      return true;
+    }
     default:
       break;
     }
